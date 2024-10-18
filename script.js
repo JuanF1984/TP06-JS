bf77e069f82a78e7bdabce2ed22998b35fce92f8
JuanF1984 <juanferreyra2684@gmail.com>
2024-10-18 11:40:12 -0300

    Trabajo práctico para practicar JavaScript

---
+++ script.js
@@ -0,0 +1,9 @@
+const nombres = ["Juan", "Ana", "Luis", "Marta", "Carlos"];
+const edades = [17, 22, 15, 25, 19];
+
+// Tarea 1: Mostrar los nombres del array al hacer clic en "Mostrar Nombres".
+
+// Tarea 2: Filtrar las personas mayores de 18 años y mostrar sus nombres en la lista al hacer clic en "Filtrar Mayores de 18".
+
+// Tarea 3: Calcular y mostrar la edad promedio de todas las personas al hacer clic en "Calcular Edad Promedio".
+
